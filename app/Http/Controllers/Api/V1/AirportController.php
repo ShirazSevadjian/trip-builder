e598e856405fad4e4314798313ab02e1d0112088
Shiraz Sevadjian <shirazsevadjian@gmail.com>
2023-05-23 23:19:37 -0400

    Added more links to API and worked on model operations

--- app/Http/Controllers/Api/V1/AirportController.php
+++ app/Http/Controllers/Api/V1/AirportController.php
@@ -14,7 +14,13 @@ class AirportController extends Controller
      */
     public function index()
     {
-        //
+        $data = Airport::all();
+
+        if($data->count() > 0){
+            return response()->json([
+                $data
+            ], 200);
+        }
     }
 
     /**
@@ -30,38 +36,163 @@ class AirportController extends Controller
      */
     public function store(StoreAirportRequest $request)
     {
-        //
+        $validator = Validator::make($request->all(), [
+            'iata' => 'required|string|max:3',
+            'name' => 'required|string|max:100',
+            'city_code' => 'required|string|max:5',
+            'country_code' => 'required|string|max:5',
+            'region_code' => 'required|string|max:5',
+            'latitude' => 'required|decimal:2,6',
+            'longitude' => 'required|decimal:2,6',
+            'timezone' => 'required|int'
+        ]);
+
+        if($validator->fails()){
+            return response()->json([
+                'status' => 422,
+                'error' => $validator->messages()
+            ], 422); //Throw a 422 error (input error)
+        }
+        else {
+            $airport = Airport::create([
+                'iata' => $request->iata,
+                'name' => $request->name,
+                'city_code' => $request->city_code,
+                'country_code' => $request->country_code,
+                'region_code' => $request->region_code,
+                'latitude' => $request->latitude,
+                'longitude' => $request->longitude,
+                'timezone' => $request->timezone
+            ]);
+        }
+
+        if($airline){
+            return response()->json([
+                'status' => 200,
+                'response' => 'Airport successfully added!'
+            ], 200);
+        } else {
+            return response()->json([
+                'status' => 500,
+                'response' => '[Failed] Airport was not added.'
+            ], 500);
+        }
     }
 
     /**
      * Display the specified resource.
      */
-    public function show(Airport $airport)
+    public function show($airport)
     {
-        //
+        $value = Airport::find($airport);
+
+        if($value){
+            return response()->json([
+                'status' => 200,
+                'airline' => $value
+            ], 200);
+        }
+        else {
+            return response()->json([
+                'status' => 404,
+                'response' => 'Airport with id ' . $airport . ' was not found!'
+            ], 404);
+        }
     }
 
     /**
      * Show the form for editing the specified resource.
      */
-    public function edit(Airport $airport)
+    public function edit($airport)
     {
-        //
+        $value = Airport::find($airport);
+
+        if($value){
+            return response()->json([
+                'status' => 200,
+                'airline' => $value
+            ], 200);
+        }
+        else {
+            return response()->json([
+                'status' => 404,
+                'response' => 'Airport with id ' . $airport . ' was not found!'
+            ], 404);
+        }
     }
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(UpdateAirportRequest $request, Airport $airport)
+    public function update(UpdateAirportRequest $request, int $id)
     {
-        //
+        $validator = Validator::make($request->all(), [
+            'iata' => 'required|string|max:3',
+            'name' => 'required|string|max:100',
+            'city_code' => 'required|string|max:5',
+            'country_code' => 'required|string|max:5',
+            'region_code' => 'required|string|max:5',
+            'latitude' => 'required|decimal:2,6',
+            'longitude' => 'required|decimal:2,6',
+            'timezone' => 'required|int'
+        ]);
+
+        if($validator->fails()){
+            return response()->json([
+                'status' => 422,
+                'error' => $validator->messages()
+            ], 422); //Throw a 422 error (input error)
+        }
+        else {
+            $airport = Airport::find($id);
+
+            if($airport){
+                $airport->update([
+                    'iata' => $request->iata,
+                    'name' => $request->name,
+                    'city_code' => $request->city_code,
+                    'country_code' => $request->country_code,
+                    'region_code' => $request->region_code,
+                    'latitude' => $request->latitude,
+                    'longitude' => $request->longitude,
+                    'timezone' => $request->timezone
+                ]);
+
+                return response()->json([
+                    'status' => 200,
+                    'response' => 'Airport successfully updated!'
+                ], 200);
+            } else {
+                return response()->json([
+                    'status' => 500,
+                    'response' => '[Failed] Airport was not updated.'
+                ], 500);
+            }
+            
+        }          
     }
 
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(Airport $airport)
+    public function destroy($id)
     {
-        //
+        $airport = Airport::find($id);
+
+        if($airport){
+
+            $airport->delete();
+
+            return response()->json([
+                'status' => 200,
+                'response' => 'Airport with id ' . $id . ' was successfully deleted.'
+            ], 200);
+        }
+        else {
+            return response()->json([
+                'status' => 404,
+                'response' => 'Airport with id ' . $id . ' was not found!'
+            ], 404);
+        }
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,12 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 use App\Http\Controllers\Api\V1\AirlineController;
+use App\Http\Controllers\Api\V1\AirportController;
+use App\Http\Controllers\Api\V1\CityCodesController;
+use App\Http\Controllers\Api\V1\CountryCodesController;
+use App\Http\Controllers\Api\V1\FlightController;
+use App\Http\Controllers\Api\V1\RegionCodesController;
+use App\Http\Controllers\Api\V1\TimezonesController;
 
 /*
 |--------------------------------------------------------------------------
@@ -24,7 +30,7 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 // In this case it will be 'api/v1/...'
 Route::group(['prefix' => 'v1', 'namespace' => 'App\Http\Controllers\Api\V1'], function() {
 
-    // Airline
+    // ---------- Airline ----------
     Route::get('airlines', [AirlineController::class, 'index']); // Show all airlines
     Route::get('airlines/{id}', [AirlineController::class, 'show']); // Show specific airline with ID
     Route::get('airlines/{id}/edit', [AirlineController::class, 'edit']); // Edit specific airline with ID
@@ -32,6 +38,57 @@ Route::group(['prefix' => 'v1', 'namespace' => 'App\Http\Controllers\Api\V1'], f
     Route::delete('airlines/{id}/delete', [AirlineController::class, 'destroy']); // Delete specific airline with ID
     Route::post('airlines', [AirlineController::class, 'store']); // Store or add new airline 
     
+
+    // ---------- Airport ----------
+    Route::get('airports', [AirportController::class, 'index']); // Show all airlports
+    Route::get('airports/{id}', [AirportController::class, 'show']); // Show specific airport with ID
+    Route::get('airports/{id}/edit', [AirportController::class, 'edit']); // Edit specific airport with ID
+    Route::put('airports/{id}/edit', [AirportController::class, 'update']); // Update specific airport with ID
+    Route::delete('airports/{id}/delete', [AirportController::class, 'destroy']); // Delete specific airport with ID
+    Route::post('airports', [AirportController::class, 'store']); // Store or add new airport 
     
 
+    // ---------- CityCode ----------
+    Route::get('citycodes', [CityCodesController::class, 'index']); // Show all citycodes
+    Route::get('citycodes/{id}', [CityCodesController::class, 'show']); // Show specific citycode with ID
+    Route::get('citycodes/{id}/edit', [CityCodesController::class, 'edit']); // Edit specific citycode with ID
+    Route::put('citycodes/{id}/edit', [CityCodesController::class, 'update']); // Update specific citycode with ID
+    Route::delete('citycodes/{id}/delete', [CityCodesController::class, 'destroy']); // Delete specific citycode with ID
+    Route::post('citycodes', [CityCodesController::class, 'store']); // Store or add new citycode 
+        
+
+    // ---------- CountryCode ----------
+    Route::get('countrycodes', [CountryCodesController::class, 'index']); // Show all countrycodes
+    Route::get('countrycodes/{id}', [CountryCodesController::class, 'show']); // Show specific countrycode with ID
+    Route::get('countrycodes/{id}/edit', [CountryCodesController::class, 'edit']); // Edit specific countrycode with ID
+    Route::put('countrycodes/{id}/edit', [CountryCodesController::class, 'update']); // Update specific countrycode with ID
+    Route::delete('countrycodes/{id}/delete', [CountryCodesController::class, 'destroy']); // Delete specific countrycode with ID
+    Route::post('countrycodes', [CountryCodesController::class, 'store']); // Store or add new countrycode 
+           
+
+    // ---------- Flight ----------
+    Route::get('flights', [FlightController::class, 'index']); // Show all flights
+    Route::get('flights/{id}', [FlightController::class, 'show']); // Show specific flight with ID
+    Route::get('flights/{id}/edit', [FlightController::class, 'edit']); // Edit specific flight with ID
+    Route::put('flights/{id}/edit', [FlightController::class, 'update']); // Update specific flight with ID
+    Route::delete('flights/{id}/delete', [FlightController::class, 'destroy']); // Delete specific flight with ID
+    Route::post('flights', [FlightController::class, 'store']); // Store or add new flight 
+
+      
+    // ---------- RegionCode ----------
+    Route::get('regioncodes', [RegionCodesController::class, 'index']); // Show all regioncodes
+    Route::get('regioncodes/{id}', [RegionCodesController::class, 'show']); // Show specific regioncode with ID
+    Route::get('regioncodes/{id}/edit', [RegionCodesController::class, 'edit']); // Edit specific regioncode with ID
+    Route::put('regioncodes/{id}/edit', [RegionCodesController::class, 'update']); // Update specific regioncode with ID
+    Route::delete('regioncodes/{id}/delete', [RegionCodesController::class, 'destroy']); // Delete specific regioncode with ID
+    Route::post('regioncodes', [RegionCodesController::class, 'store']); // Store or add new regioncode 
+         
+
+    // ---------- Timezone ----------
+    Route::get('timezones', [TimezonesController::class, 'index']); // Show all timezones
+    Route::get('timezones/{id}', [TimezonesController::class, 'show']); // Show specific timezone with ID
+    Route::get('timezones/{id}/edit', [TimezonesController::class, 'edit']); // Edit specific timezone with ID
+    Route::put('timezones/{id}/edit', [TimezonesController::class, 'update']); // Update specific timezone with ID
+    Route::delete('timezones/{id}/delete', [TimezonesController::class, 'destroy']); // Delete specific timezone with ID
+    Route::post('timezones', [TimezonesController::class, 'store']); // Store or add new timezone 
 });
